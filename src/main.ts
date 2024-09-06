fc5847d84c1b0895c57e527fc7f52be7083f7e73
Gordon <>
2024-09-06 16:44:51 -0400

    chore: Update workflow config.yml to add test_token permission

--- src/main.ts
+++ src/main.ts
@@ -34,6 +34,40 @@ async function validate_oci_cli_installed_and_configured() {
   }
 }
 
+async function configure_oci_cli(privateKey: crypto.KeyObject, publicKey: crypto.KeyObject, upstToken: string, ociUser: string, ociFingerprint: string, ociTenancy: string, ociRegion: string) {
+  // Setup and Initialization OCI CLI Profile
+  const workspace = process.env.GITHUB_WORKSPACE || '';
+  const ociConfigDir = path.join(workspace, '.oci');
+  const ociConfigFile = path.join(ociConfigDir, 'config');
+  const ociPrivateKeyFile = path.join(workspace, 'private_key.pem');
+  const ociPublicKeyFile = path.join(workspace, 'public_key.pem');
+  const upstTokenFile = path.join(workspace, 'session');
+  const ociConfig = `[DEFAULT]
+  user=${ociUser}
+  fingerprint=${ociFingerprint}
+  key_file=${ociPrivateKeyFile}
+  tenancy=${ociTenancy}
+  region=${ociRegion}
+  security_token=${upstToken}
+  `;
+
+  // Create the .oci directory
+  await io.mkdirP(ociConfigDir);
+
+  // Write the OCI config file
+  fs.writeFileSync(ociConfigFile, ociConfig);
+
+  // Write the private key file
+  fs.writeFileSync(ociPrivateKeyFile, privateKey.export({ type: 'pkcs1', format: 'pem' }) as string);
+
+  // Write the public key file
+  fs.writeFileSync(ociPublicKeyFile, publicKey.export({ type: 'spki', format: 'pem' }) as string);
+  
+  // Write the UPST token to the file system
+  fs.writeFileSync(upstTokenFile, upstToken);
+  
+}
+
 async function token_exchange_jwt_to_upst(token_exchange_url: string, client_cred: string, oci_public_key: string, subject_token: string): Promise<any> {
   const headers = {
       'Content-Type': 'application/x-www-form-urlencoded',
@@ -63,8 +97,7 @@ async function run(): Promise<void> {
     const ociUser = core.getInput('oci_user', { required: true });
     const ociTenancy = core.getInput('oci_tenancy', { required: true });
     const ociRegion = core.getInput('oci_region', { required: true });
-  
-
+    const testToken = core.getInput('test_token', { required: true });
 
     // Get github OIDC JWT token
     const idToken = await core.getIDToken();
@@ -77,11 +110,13 @@ async function run(): Promise<void> {
     let clientCreds = `${clientId}:${clientSecret}`;
     let authStringEncoded = Buffer.from(clientCreds).toString('base64');
     const ociFingerprint = await calc_fingerprint(publicKey);
+
     // Get the B64 encoded public key DER
     let publicKeyB64 = publicKey.export({ type: 'spki', format: 'der' }).toString('base64');
     console.info(`Public Key B64: ${publicKeyB64}`);
+
     //Exchange JWT to UPST
-    let upstToken = await token_exchange_jwt_to_upst(`${domainBaseURL}/oauth2/v1/token`, authStringEncoded, publicKeyB64, idToken);
+    let upstToken = await token_exchange_jwt_to_upst(`${domainBaseURL}/oauth2/v1/token`, authStringEncoded, publicKeyB64, testToken?testToken : idToken);
     console.log(`UPST Token:  ${upstToken}`);
     await configure_oci_cli(privateKey, publicKey, upstToken.access_token, ociUser, ociFingerprint, ociTenancy, ociRegion);
 
@@ -92,32 +127,3 @@ async function run(): Promise<void> {
 }
 
 run();
-
-async function configure_oci_cli(privateKey: crypto.KeyObject, publicKey: crypto.KeyObject, upstToken: string, ociUser: string, ociFingerprint: string, ociTenancy: string, ociRegion: string) {
-  // Setup and Initialization OCI CLI Profile
-  const workspace = process.env.GITHUB_WORKSPACE || '';
-  const ociConfigDir = path.join(workspace, '.oci');
-  const ociConfigFile = path.join(ociConfigDir, 'config');
-  const ociPrivateKeyFile = path.join(workspace, 'private_key.pem');
-  const ociPublicKeyFile = path.join(workspace, 'public_key.pem');
-  const upstTokenFile = path.join(workspace, 'session');
-  const ociConfig = `[DEFAULT]
-  user=${ociUser}
-  fingerprint=${ociFingerprint}
-  key_file=${ociPrivateKeyFile}
-  tenancy=${ociTenancy}
-  region=${ociRegion}
-  security_token=${upstToken}
-  `;
-  // Create the .oci directory
-  await io.mkdirP(ociConfigDir);
-  // Write the OCI config file
-  fs.writeFileSync(ociConfigFile, ociConfig);
-  // Write the private key file
-  fs.writeFileSync(ociPrivateKeyFile, privateKey.export({ type: 'pkcs1', format: 'pem' }) as string);
-  // Write the public key file
-  fs.writeFileSync(ociPublicKeyFile, publicKey.export({ type: 'spki', format: 'pem' }) as string);
-  // Write the UPST token to the file system
-  fs.writeFileSync(upstTokenFile, upstToken);
-  
-}
