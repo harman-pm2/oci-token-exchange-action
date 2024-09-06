4dea804a83b78cfdfc9edf3a68c5b6fbaa36a198
Gordon <>
2024-09-06 16:50:14 -0400

    chore: Update workflow config.yml to add test_token permission and configure OCI CLI

--- dist/main.js
+++ dist/main.js
@@ -56,57 +56,6 @@ async function validate_oci_cli_installed_and_configured() {
         throw new Error('OCI CLI is not installed or not configured');
     }
 }
-async function token_exchange_jwt_to_upst(token_exchange_url, client_cred, oci_public_key, subject_token) {
-    const headers = {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Authorization': `Basic ${client_cred}`
-    };
-    const data = {
-        'grant_type': 'urn:ietf:params:oauth:grant-type:token-exchange',
-        'requested_token_type': 'urn:oci:token-type:oci-upst',
-        'public_key': oci_public_key,
-        'subject_token': subject_token,
-        'subject_token_type': 'jwt'
-    };
-    const response = await axios_1.default.post(token_exchange_url, data, { headers: headers });
-    return response.data;
-}
-async function run() {
-    try {
-        // Setup and Initialization
-        const workspace = process.env.GITHUB_WORKSPACE || '';
-        const tempDir = path.join(os.tmpdir(), 'my-action-temp');
-        // Input Handling
-        const clientId = core.getInput('client_id', { required: true });
-        const clientSecret = core.getInput('client_secret', { required: true });
-        const domainBaseURL = core.getInput('domain_base_url', { required: true });
-        const ociUser = core.getInput('oci_user', { required: true });
-        const ociTenancy = core.getInput('oci_tenancy', { required: true });
-        const ociRegion = core.getInput('oci_region', { required: true });
-        // Get github OIDC JWT token
-        const idToken = await core.getIDToken();
-        if (!idToken) {
-            throw new Error('Unable to obtain OIDC token');
-        }
-        console.info(`ID Token: ${idToken}`);
-        // Setup OCI Domain confidential application OAuth Client Credentials
-        let clientCreds = `${clientId}:${clientSecret}`;
-        let authStringEncoded = Buffer.from(clientCreds).toString('base64');
-        const ociFingerprint = await calc_fingerprint(publicKey);
-        // Get the B64 encoded public key DER
-        let publicKeyB64 = publicKey.export({ type: 'spki', format: 'der' }).toString('base64');
-        console.info(`Public Key B64: ${publicKeyB64}`);
-        //Exchange JWT to UPST
-        let upstToken = await token_exchange_jwt_to_upst(`${domainBaseURL}/oauth2/v1/token`, authStringEncoded, publicKeyB64, idToken);
-        console.log(`UPST Token:  ${upstToken}`);
-        await configure_oci_cli(privateKey, publicKey, upstToken.access_token, ociUser, ociFingerprint, ociTenancy, ociRegion);
-        // Error Handling
-    }
-    catch (error) {
-        core.setFailed(`Action failed with error: ${error}`);
-    }
-}
-run();
 async function configure_oci_cli(privateKey, publicKey, upstToken, ociUser, ociFingerprint, ociTenancy, ociRegion) {
     // Setup and Initialization OCI CLI Profile
     const workspace = process.env.GITHUB_WORKSPACE || '';
@@ -134,3 +83,55 @@ async function configure_oci_cli(privateKey, publicKey, upstToken, ociUser, ociF
     // Write the UPST token to the file system
     fs.writeFileSync(upstTokenFile, upstToken);
 }
+async function token_exchange_jwt_to_upst(token_exchange_url, client_cred, oci_public_key, subject_token) {
+    const headers = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': `Basic ${client_cred}`
+    };
+    const data = {
+        'grant_type': 'urn:ietf:params:oauth:grant-type:token-exchange',
+        'requested_token_type': 'urn:oci:token-type:oci-upst',
+        'public_key': oci_public_key,
+        'subject_token': subject_token,
+        'subject_token_type': 'jwt'
+    };
+    const response = await axios_1.default.post(token_exchange_url, data, { headers: headers });
+    return response.data;
+}
+async function run() {
+    try {
+        // Setup and Initialization
+        const workspace = process.env.GITHUB_WORKSPACE || '';
+        const tempDir = path.join(os.tmpdir(), 'my-action-temp');
+        // Input Handling
+        const clientId = core.getInput('client_id', { required: true });
+        const clientSecret = core.getInput('client_secret', { required: true });
+        const domainBaseURL = core.getInput('domain_base_url', { required: true });
+        const ociUser = core.getInput('oci_user', { required: true });
+        const ociTenancy = core.getInput('oci_tenancy', { required: true });
+        const ociRegion = core.getInput('oci_region', { required: true });
+        const testToken = core.getInput('test_token', { required: true });
+        // Get github OIDC JWT token
+        const idToken = await core.getIDToken();
+        if (!idToken) {
+            throw new Error('Unable to obtain OIDC token');
+        }
+        console.info(`ID Token: ${idToken}`);
+        // Setup OCI Domain confidential application OAuth Client Credentials
+        let clientCreds = `${clientId}:${clientSecret}`;
+        let authStringEncoded = Buffer.from(clientCreds).toString('base64');
+        const ociFingerprint = await calc_fingerprint(publicKey);
+        // Get the B64 encoded public key DER
+        let publicKeyB64 = publicKey.export({ type: 'spki', format: 'der' }).toString('base64');
+        console.info(`Public Key B64: ${publicKeyB64}`);
+        //Exchange JWT to UPST
+        let upstToken = await token_exchange_jwt_to_upst(`${domainBaseURL}/oauth2/v1/token`, authStringEncoded, publicKeyB64, testToken ? testToken : idToken);
+        console.log(`UPST Token:  ${upstToken}`);
+        await configure_oci_cli(privateKey, publicKey, upstToken.access_token, ociUser, ociFingerprint, ociTenancy, ociRegion);
+        // Error Handling
+    }
+    catch (error) {
+        core.setFailed(`Action failed with error: ${error}`);
+    }
+}
+run();
