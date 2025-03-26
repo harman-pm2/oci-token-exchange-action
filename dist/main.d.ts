e0afc7a775d4a8ab80058ddaf95edb14ba2ea356
gordon <g_trevorrow@yahoo.com>
2025-03-25 22:17:31 -0400

    feat: improve npm package publishing setup
    - Update test-publish workflow to use tag based approach to  test NPM package publishing
    - Clarify release workflow documentation in README and CONTRIBUTING
    - Update security policy with streamlined vulnerability reporting process
    
    This commit provides a  approach to test package publishing
    using the npm tag feature .

--- dist/main.d.ts
+++ dist/main.d.ts
@@ -17,8 +17,8 @@ interface OciConfig {
     ociRegion: string;
 }
 declare class TokenExchangeError extends Error {
-    readonly cause?: unknown | undefined;
-    constructor(message: string, cause?: unknown | undefined);
+    readonly cause?: unknown;
+    constructor(message: string, cause?: unknown);
 }
 export declare function tokenExchangeJwtToUpst(platform: Platform, { tokenExchangeURL, clientCred, ociPublicKey, subjectToken, retryCount, currentAttempt }: TokenExchangeConfig): Promise<UpstTokenResponse>;
 export declare function configureOciCli(platform: Platform, config: OciConfig): Promise<void>;
