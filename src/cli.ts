0dda7af0d923147438bb3a8f0e37841d3b6ca654
gordon <g_trevorrow@yahoo.com>
2025-04-17 02:14:14 -0400

    chore: update copyright year to 2025
    Update the copyright year in the header comments of source files.
    
    Signed-off-by: gordon <g_trevorrow@yahoo.com>

--- src/cli.ts
+++ src/cli.ts
@@ -1,5 +1,9 @@
 #!/usr/bin/env node
 
+/**
+ * Copyright (c) 2021, 2025 Oracle and/or its affiliates.
+ * Licensed under the Universal Permissive License v1.0 as shown at https://oss.oracle.com/licenses/upl.
+ */
 import { main } from './main';
 
 // Map environment variables to GitHub Actions input format
